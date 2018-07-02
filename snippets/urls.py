29a20009d20c5d85526d9d1276e92dccc84ad25a
Christopher Zielinski <ckz8780@gmail.com>
2018-07-02 17:49:40 -0400

    Added coreapi and coreapi-cli, and set up schema view

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from snippets import views
+from rest_framework.schemas import get_schema_view
+
+# For automatic schema generation
+schema_view = get_schema_view(title='DRF Pastebin')
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -9,5 +13,6 @@ router.register(r'users', views.UserViewSet)
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('schema/', schema_view)
 ]
