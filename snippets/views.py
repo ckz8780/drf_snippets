833a346a99b076508db9b6d9f6f8b20313331f27
Christopher Zielinski <ckz8780@gmail.com>
2018-07-02 14:00:28 -0400

    Added users endpoints and auth views

--- snippets/views.py
+++ snippets/views.py
@@ -1,16 +1,26 @@
 from django.contrib.auth.models import User
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    # Ensure only auth'd users can create/update snippets
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    # Pass an additional 'owner' field to the serializer upon saving
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+    # Ensure only auth'd users can create/update snippets
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
